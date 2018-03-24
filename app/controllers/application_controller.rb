329a4a8b91946d03f67beb81ed1a676dd00feb9e
raluca <dreve_raluca@yahoo.com>
2018-03-24 15:03:45 +0200

    updates

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  def url_path
+  	request.fullpath
+  end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+	def active_url(path)
+  		'active' if request.fullpath == path
+  	end
 end
